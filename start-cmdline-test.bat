ce68e08cc3ba106309b4e2c0080c256727c3f408
janvarev <janvarev@gmail.com>
2023-04-13 10:44:17 +0300

    initial commit

---
+++ start-cmdline-test.bat
@@ -0,0 +1,19 @@
+@echo off
+
+@echo Starting the Irene Voice Assistant (cmdline test)...
+
+cd /D "%~dp0"
+
+set MAMBA_ROOT_PREFIX=%cd%\installer_files\mamba
+set INSTALL_ENV_DIR=%cd%\installer_files\env
+
+if not exist "%MAMBA_ROOT_PREFIX%\condabin\micromamba.bat" (
+  call "%MAMBA_ROOT_PREFIX%\micromamba.exe" shell hook >nul 2>&1
+)
+call "%MAMBA_ROOT_PREFIX%\condabin\micromamba.bat" activate "%INSTALL_ENV_DIR%" || ( echo MicroMamba hook not found. && goto end )
+cd Irene-Voice-Assistant
+
+call python runva_cmdline.py
+
+:end
+pause
